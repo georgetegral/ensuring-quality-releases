d26af42ac9954d71900bde5d95ec47e9d773acf4
georgetegral <georgetegral@gmail.com>
2021-07-07 14:11:55 -0500

    Added login in seleniumtest

---
+++ automatedtesting/selenium/selenium-test.py
@@ -0,0 +1,25 @@
+# #!/usr/bin/env python
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options as ChromeOptions
+
+
+# Start the browser and login with standard_user
+def login (user, password):
+    print ('Starting the browser...')
+    # --uncomment when running in Azure DevOps.
+    options = ChromeOptions()
+    options.add_argument("--headless") 
+    driver = webdriver.Chrome(options=options)
+    driver = webdriver.Chrome()
+    print ('Browser started successfully. Navigating to the demo page to login.')
+    driver.get('https://www.saucedemo.com/')
+    driver.find_element_by_css_selector("input[id='user-name']").send_keys(user)
+    driver.find_element_by_css_selector("input[id='password']").send_keys(password)
+    driver.find_element_by_id("login-button").click()
+    product_label = driver.find_element_by_css_selector("div[class='product_label']").text
+    assert "Products" in product_label
+    print(timestamp() + 'Login with username {:s} and password {:s} successfully.'.format(user, password))
+    return driver
+
+login('standard_user', 'secret_sauce')
+
